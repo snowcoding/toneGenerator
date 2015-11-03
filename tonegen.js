866a6b21b38db41fa227990bfcac610f07415df4
snowcoding <vimalshah77@gmail.com>
2015-11-02 17:23:05 -0800

    Boiler plate JS

---
+++ tonegen.js
@@ -0,0 +1,24 @@
+//Add Event Listeners
+document.getElementById("startTone").addEventListener("click", startTone);
+document.getElementById("stopTone").addEventListener("click", stopTone);
+
+//Basic start/stop functions
+function startTone() {
+    //Initialize the AudioContext Object and connect to destination
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    oscillator = audioCtx.createOscillator();
+    var gainNode = audioCtx.createGain();
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+
+    //Initialize the Oscillator
+    oscillator.type = 'sine'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
+    oscillator.frequency.value = 432; // value in hertz
+    oscillator.start();
+}
+
+function stopTone() {
+    oscillator.stop();
+    oscillator.disconnect();
+}
