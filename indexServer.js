9082164bc0228388894c65b18ceece54ec8c0982
Naveen Neelakantam <neelakan@gmail.com>
2015-11-28 11:04:41 -0800

    add indexServer.js. Use 'node indexServer.js' to serve up the app as a website

---
+++ indexServer.js
@@ -0,0 +1,11 @@
+// Simple web server to provide index.html to other clients on the network
+var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var path = require('path');
+var port = 1988;
+
+// viewed at http://localhost:port
+app.use(express.static('.'));
+
+app.listen(1988);
